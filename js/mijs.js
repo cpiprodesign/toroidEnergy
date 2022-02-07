5830e745bb425e82e55503451cc1f75de835bcff
Cristian <cristian@cpiprodesig.com>
2022-02-06 21:33:00 -0500

    animado contador

---
+++ js/mijs.js
@@ -0,0 +1,25 @@
+addEventListener('DOMContentLoaded',()=>{
+    const contadores=document.querySelectorAll('.contador_cantidad')
+    const velocidad=100
+
+    const animarContadores =()=>{
+        for (const contador of contadores){
+            const actualizar_contador =()=>{
+                let cantidad_maxima= +contador.dataset.cantidadTotal,
+                valor_actual =+contador.innerText,
+                incremento = cantidad_maxima/velocidad
+                if(valor_actual < cantidad_maxima){
+                    contador.innerText = Math.ceil(valor_actual+incremento)
+                    setTimeout(actualizar_contador,5)
+                }
+                else{
+                    contador.innerText=cantidad_maxima
+
+                }
+            }
+            actualizar_contador()
+        }
+       
+    }
+    animarContadores()
+})
